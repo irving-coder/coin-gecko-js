86cad4100f2b825705389819cfd65b7824211d8d
Irving Martínez <irving.mtqz10@yahoo.com>
2022-01-04 23:22:16 -0600

    Agregando archivo de estilos y mas estructura al html

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
-'use strict'
+(function(){
+    'use strict'
 
-console.log('Hola mundo. Creación del proyecto CoinGecko');
+    console.log('Hola mundo. Creación del proyecto CoinGecko');
+    
+    // https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
+})();
