8b679848121a4b098204a056ac6ac85728fa3634
Irving Martínez <irving.mtqz10@yahoo.com>
2022-01-08 23:39:46 -0600

    Imprimiendo id en la tabla

--- js/main.js
+++ js/main.js
@@ -3,14 +3,19 @@
 
     let mainTable = document.querySelector('#main-table');
 
-    mainTable.innerHTML = '<th id="id-number">#</th> <th id="coin">Coin</th> <th  id="price">Price</th> <th id="price-ch">Price Change</th> <th id="h24">24h Volume</th>';
+    
 
-    // fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
-    //     .then(response => response.json())
-    //     .then(data => {
-    //         data.map((elem, index) => {
+    fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+        .then(response => response.json())
+        .then(data => {
+            data.map((elem, index) => {
+                var tr = document.createElement('tr');
+                var td = document.createElement('td');
 
-    //             console.log(elem.id);
-    //         });
-    //     })
+                td.innerHTML = elem.id;
+                tr.append(td);
+
+                mainTable.appendChild(tr);
+            });
+        })
 })();
