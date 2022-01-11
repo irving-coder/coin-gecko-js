3695eb0b6ce5786316f6f5ac113303bcc4641574
Irving Martínez <irving.mtqz10@yahoo.com>
2022-01-10 23:17:17 -0600

    Aumentando espacio entre filas

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 (function(){
     'use strict'
 
-    let mainTable = document.querySelector('#main-table');
-
-    
+    // let mainTable = document.querySelector('#main-table');
+    let tbody = document.querySelector('tbody');
 
     fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
         .then(response => response.json())
@@ -15,7 +14,7 @@
                 td = '<td>' +(index +  1)+ '</td><td>' +elem.id+ '</td> <td>' +elem.current_price.toLocaleString()+ '</td> <td>' +elem.price_change_percentage_24h+ '</td> <td>' +elem.total_volume.toLocaleString()+ '</td>';
 
                 tr.innerHTML = td;
-                mainTable.appendChild(tr);
+                tbody.appendChild(tr);
             });
         })
 })();
